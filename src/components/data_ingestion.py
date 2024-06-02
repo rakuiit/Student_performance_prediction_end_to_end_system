cc91377ba84e82f04db0c16a1d4de1fef958491f
rakuiit <en.rakeshsharma@gmail.com>
2024-06-02 17:18:13 +0530

    data tranformation 2

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path:str=os.path.join('artifacts',"train.csv")
@@ -42,6 +46,11 @@ class DataIngestion:
 
 # if __name__=="__main__":
 #     obj=DataIngestion()
-#     obj.initiate_data_ingestion()
+#     train_data,test_data=obj.initiate_data_ingestion()
+
+#     data_tranformation=DataTransformation()
+#     print(train_data)
+#     print(test_data)
+#     data_tranformation.initiate_data_transformation(train_data,test_data)
 
 
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -11,7 +11,7 @@ from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
-
+from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
@@ -39,10 +39,9 @@ class DataTransformation:
                                    ]
                                    )
             cat_pipeline=Pipeline(steps=
-                                  [("imputer",SimpleImputer(strategy='most frequent'))
-                                   ("one_hot_encoder",OneHotEncoder()),
-                                   ("scaler",StandardScaler())])
-            
+                                  [("imputer",SimpleImputer(strategy='most_frequent')),
+                                   ("one_hot_encoder",OneHotEncoder())
+                                  ])
             logging.info("num and cat variable standard scaling completed")
 
             ### apply on data frame
@@ -65,8 +64,40 @@ class DataTransformation:
         try:
             train_df=pd.read_csv(train_path)
             test_df=pd.read_csv(test_path)
+            print(train_df.head())
+            print(test_df.head())
+            
+            logging.info("Read train and test data completed")
 
-        except:
+            target_column_name="math_score"
+
+            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df=train_df[target_column_name]
+
+            
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
+
+            logging.info("applying preprocessing object on training and testing dataframe")
+
+            preprocessing_obj=self.get_data_transformer_object()
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+
+
+            train_arr=np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
+            test_arr=np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
+
+            save_object(file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                        obj=preprocessing_obj)
+
+            return (train_arr,
+                    test_arr,
+                    self.data_transformation_config.preprocessor_obj_file_path)
+                    
+
+        except Exception as e:
+            raise CustomException(e,sys)
 
 
 
