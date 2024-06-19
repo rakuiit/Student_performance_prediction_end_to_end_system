30bae4c04ca36b7704016c070fc5b70864df34ac
rakuiit <en.rakeshsharma@gmail.com>
2024-06-19 19:45:26 +0530

    changes

--- app.py
+++ app.py
@@ -49,7 +49,8 @@ def predict_datapoint():
 if __name__ == '__main__':
     #app.run(debug=True)
     #app.run(host='127.0.0.1',port=8080,debug=True)
-    app.run(host="0.0.0.0",port=8080)
+    #app.run(host="0.0.0.0",port=8080)
+    app.run(host='0.0.0.0', port=8080)  
 
 
 
